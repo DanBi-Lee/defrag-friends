b098490c3724585e5b89efaf3588880c00d99080
DanBi-Lee <danbi.s.rain@gmail.com>
2021-01-28 20:14:12 +0900

    firebase 패키지 추가

---
+++ src/service/auth_service.js
@@ -0,0 +1,20 @@
+import firebase from "firebase/app";
+import firebaseApp from "./firebase";
+import "firebase/auth";
+
+class AuthService {
+  loginWidthEmail(email, password) {
+    return firebaseApp.auth().signInWithEmailAndPassword(email, password);
+  }
+
+  loginWidthSNS(providerName) {
+    const authProvider = new firebase.auth[`${providerName}AuthProvider`]();
+    return firebaseApp.auth().signInWithPopup(authProvider);
+  }
+
+  joinWidthEmail(email, password) {
+    return firebaseApp.auth().createUserWithEmailAndPassword(email, password);
+  }
+}
+
+export default AuthService;
